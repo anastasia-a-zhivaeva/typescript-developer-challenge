4b9599420332e37ea46f798f7f742e3be1993be2
anastasia-a-zhivaeva <anastasia.a.zhivaeva@gmail.com>
2021-01-26 18:50:15 +0300

    Added task and solution, Updated README, Installed packages

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # typescript-developer-challenge
 Software Developer challenge
+
+I solved this during an interview for the position of JavaScript Developer.
+You can find the task in `index.ts` and the solution in `solution.ts`.
+
+Try to solve it and then compare our results 😉
---
+++ index.ts
@@ -0,0 +1,102 @@
+// Software developer challenge
+
+type Context = {
+  [k: string]: string | undefined
+}
+
+enum LogicalOperator {
+  And = 'AND',
+  Or = 'OR'
+}
+
+enum ComparisonOperator {
+  Eq = '=='
+}
+
+type Operator = LogicalOperator | ComparisonOperator
+type LogicalCondition = [LogicalOperator, ...Condition[]]
+type Variable = string
+type Value = string
+type ComparisonCondition = [ComparisonOperator, Variable, Value]
+type Condition = LogicalCondition | ComparisonCondition
+
+function isLogicalCondition (condition: Condition): condition is LogicalCondition {
+  return Object.values(LogicalOperator).includes(condition[0] as LogicalOperator)
+}
+
+function isComparisonCondition (condition: Condition): condition is ComparisonCondition {
+  return Object.values(ComparisonOperator).includes(condition[0] as ComparisonOperator)
+}
+
+/**
+ * Evaluate a condition against the context.
+ *
+ * Your task is to implement this function such that the
+ * test cases below pass. The solution will be graded on
+ * readability, completeness, and your ability to read
+ * code.  We're not looking for you to write much code.
+ * No additional types, interfaces, classes or functions
+ * are required.  Recommended time: 30-45 minutes.
+ *
+ * @param condition DSL condition modelled after S-expressions
+ * @param context key/value pairs
+ * @return boolean
+ */
+function evaluate (condition: Condition, context: Context): boolean {
+  return Math.random() >= 0.5
+}
+
+/*
+  A condition modelled after S-expressions.  This condition is composed of nested arrays.
+
+  The first element in any array is one of either a logical operator or comparison operator.
+
+  A logical operator is either 'OR' or 'AND'.
+  - There are 1 or more operands that proceed it.
+
+  A comparison operator is '=='.
+  - There are two operands that proceed it.
+  - The first operand is a variable indicated by the '$' sign.
+  - The second operand is a string value.
+*/
+const condition = [
+  'OR',
+  [
+    'AND',
+    ['==', '$State', 'Alabama'],
+    ['==', '$Profession', 'Software development']
+  ],
+  ['==', '$Undefined', ''],
+  [
+    'AND',
+    ['==', '$State', 'Texas']
+  ],
+  [
+    'OR',
+    [
+      'OR',
+      ['==', '$Profession', 'Tradesperson']
+    ]
+  ]
+]
+
+/**
+ * Click "Run" to execute the test cases.
+ */
+const testCases = [
+  [{'State': 'Alabama', 'Profession': 'Software development'}, true],
+  [{'State': 'Texas'}, true],
+  [{'State': 'Alabama', 'Profession': 'Gaming'}, false],
+  [{'State': 'Utah'}, false],
+  [{'Profession': 'Town crier'}, false],
+  [{'Profession': 'Tradesperson'}, true],
+  [{}, false]
+]
+
+for (const [index, [context, expected]] of testCases.entries()) {
+  console.log(
+    evaluate(condition as Condition, context) === expected
+      ? `${index} ok`
+      : `${index} FAIL`
+  )
+}
